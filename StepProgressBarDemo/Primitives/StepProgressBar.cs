f9d45bd816a773654f66b71978a6dd549f3fe825
guoyiwei <guoyiwei@syspetro.com>
2023-03-04 15:54:32 +0800

    提交初版步骤指引条

---
+++ StepProgressBarDemo/Primitives/StepProgressBar.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace StepProgressBarDemo.Primitives
+{
+    /// <summary>
+    /// 步骤指引条控件
+    /// </summary>
+    public class StepProgressBar : ListBox
+    {
+        static StepProgressBar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(StepProgressBar), new FrameworkPropertyMetadata(typeof(StepProgressBar)));
+        }
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            if (element is StepProgressBarItem stepItem)
+            {
+                var index = ItemContainerGenerator.IndexFromContainer(element);
+                if (index == Items.Count - 1)
+                {
+                    stepItem.IsLastItem = true;
+                }
+                else
+                {
+                    stepItem.IsLastItem = false;
+                }
+            }
+            base.PrepareContainerForItemOverride(element, item);
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new StepProgressBarItem();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+        protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnItemsChanged(e);
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    {
+                        if (e.NewStartingIndex == 0)//如果新添加项是放在第一位，则更改原来的第一位的属性值
+                        {
+                            SetIsLastItem(e.NewStartingIndex, Items.Count);
+                        }
+
+                        if (e.NewStartingIndex == Items.Count - e.NewItems.Count)//如果新添加项是放在最后一位，则更改原来的最后一位的属性值
+                        {
+                            SetIsLastItem(e.NewStartingIndex - 1, Items.Count);
+                        }
+                        break;
+                    }
+                case NotifyCollectionChangedAction.Remove:
+                    {
+                        if (e.OldStartingIndex == 0)//如果移除的是第一个，则更改更新后的第一项的属性值
+                        {
+                            SetIsLastItem(0, Items.Count);
+                        }
+                        else
+                        {
+                            SetIsLastItem(e.OldStartingIndex - 1, Items.Count);
+                        }
+                        break;
+                    }
+                case NotifyCollectionChangedAction.Reset:
+                    {
+                        for (int i = 0; i < Items.Count; i++)
+                        {
+                            SetIsLastItem(i, Items.Count);
+                        }
+                        break;
+                    }
+            }
+        }
+
+        private void SetIsLastItem(int index, int count)
+        {
+            if (index < count)
+            {
+                if (ItemContainerGenerator.ContainerFromIndex(index) is StepProgressBarItem stepItem)
+                {
+                    stepItem.IsLastItem = index == count - 1;
+                }
+            }
+        }
+    }
+}
---
+++ StepProgressBarDemo/Primitives/StepProgressBarItem.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace StepProgressBarDemo.Primitives
+{
+    /// <summary>
+    /// 步骤指引条子项
+    /// </summary>
+    public class StepProgressBarItem : ListBoxItem
+    {
+        static StepProgressBarItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(StepProgressBarItem), new FrameworkPropertyMetadata(typeof(StepProgressBarItem)));
+        }
+
+        [Bindable(true), Description("获取或者设置该节点是否就绪")]
+        public bool IsDone
+        {
+            get { return (bool)GetValue(IsDoneProperty); }
+            set { SetValue(IsDoneProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsDone.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsDoneProperty =
+            DependencyProperty.Register("IsDone", typeof(bool), typeof(StepProgressBarItem), new PropertyMetadata(false));
+
+
+
+
+        [Bindable(true), Description("获取或者设置该节点的就绪图片")]
+        public ImageSource DoneImageSource
+        {
+            get { return (ImageSource)GetValue(DoneImageSourceProperty); }
+            set { SetValue(DoneImageSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DoneImageSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DoneImageSourceProperty =
+            DependencyProperty.Register("DoneImageSource", typeof(ImageSource), typeof(StepProgressBarItem), new PropertyMetadata(null));
+
+
+
+        [Bindable(true), Description("获取或者设置该节点的就绪图片")]
+        public ImageSource UndoneImageSource
+        {
+            get { return (ImageSource)GetValue(UndoneImageSourceProperty); }
+            set { SetValue(UndoneImageSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for UndoneImageSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UndoneImageSourceProperty =
+            DependencyProperty.Register("UndoneImageSource", typeof(ImageSource), typeof(StepProgressBarItem), new PropertyMetadata(null));
+
+
+
+
+
+
+        public double ImageWidth
+        {
+            get { return (double)GetValue(ImageWidthProperty); }
+            set { SetValue(ImageWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageWidthProperty =
+            DependencyProperty.Register("ImageWidth", typeof(double), typeof(StepProgressBarItem), new PropertyMetadata(20.0));
+
+
+
+        public double ImageHeight
+        {
+            get { return (double)GetValue(ImageHeightProperty); }
+            set { SetValue(ImageHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageHeightProperty =
+            DependencyProperty.Register("ImageHeight", typeof(double), typeof(StepProgressBarItem), new PropertyMetadata(20.0));
+
+
+
+
+
+
+
+
+
+
+        [Bindable(true), Description("获取或者设置该节点是否为最后一个步骤节点")]
+        public bool IsLastItem
+        {
+            get { return (bool)GetValue(IsLastItemProperty); }
+            set { SetValue(IsLastItemProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsLastItem.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsLastItemProperty =
+            DependencyProperty.Register("IsLastItem", typeof(bool), typeof(StepProgressBarItem), new PropertyMetadata(true));
+
+
+
+    }
+}
